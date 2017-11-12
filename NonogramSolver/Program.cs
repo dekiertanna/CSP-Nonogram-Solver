cc2584d3de0dfd7007318ca0921b3a5c8992e07c
dekiertanna <dekiertanna@gmail.com>
2017-11-12 12:26:27 +0100

    working backtracking with domain heuristic

--- NonogramSolver/Program.cs
+++ NonogramSolver/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace NonogramSolver
         {
             string line;
 
-            System.IO.StreamReader file = new System.IO.StreamReader("picture.txt");
+            System.IO.StreamReader file = new System.IO.StreamReader("serce.txt");
 
             int numOfRows = Convert.ToInt32(file.ReadLine());
             int numOfCols = Convert.ToInt32(file.ReadLine());
@@ -93,7 +94,7 @@ namespace NonogramSolver
             }
 
             List<Line> variables = new List<Line>();
-            List<Line> assignment = new List<Line>();
+            List<Solution> assignment = new List<Solution>();
 
             foreach (Line l in grid.rows)
             {
@@ -103,74 +104,134 @@ namespace NonogramSolver
             {
                 variables.Add(l);
             }
+            List<bool> defaultSolution = new List<bool>();
+            foreach(Line l in variables)
+            {
+                foreach(bool b in l.possibleSolutions[0])
+                {
+                    defaultSolution.Add(false);
+                }
 
+                assignment.Add(new Solution(defaultSolution, l.lineType, l.index));
+                defaultSolution.Clear();
+            }
+            variables = variables.OrderBy(o => o.possibleSolutions.Count).ToList();
+            List<Line> safeVars = new List<Line>(variables);
 
-            bool valid(LineType lt, int index, List<bool> possibleSolution, List<Line> solution)
+            foreach(Line l in safeVars)
+            {
+                foreach(List<bool> b in l.possibleSolutions)
+                {
+                    foreach(bool bb in b)
+                    {
+                        Console.WriteLine(bb);
+                    }
+                }
+            }
+
+            bool valid(LineType lt, int index, List<bool> possibleSolution, List<Solution> solution)
             {
                 for (int i = 0; i < solution.Count; i++)
                 {
-                    Line temp = solution[i];
-                    if (temp.lineType != lt)
+                    Solution temp = solution[i];
+                    if (temp.lineType != lt && temp.assigned)
                     {
 
-                        if (temp.currentSolution[index] != possibleSolution[temp.index])
+                        if (temp.solution[index] != possibleSolution[temp.index])
                         {
                             return false;
                         }
                     }
 
-               }
+                }
                 return true;
             }
-
-            bool backtrack(List<Line> vars, int level, List<Line> solution)
+            System.IO.StreamWriter fileTest = new System.IO.StreamWriter("testing.txt",true);
+            bool forwardChecking(List<Line> vars,List<bool> value,LineType lt, int index)
             {
+                for(int i=0;i<vars.Count;i++)
+                {
+                    if(vars[i].lineType!=lt)
+                    {
+                        List<List<bool>> possibleSolutionsCopy = new List<List<bool>>();
+                        for(int z=0;z<vars[i].possibleSolutions.Count;z++)
+                        {
+                            possibleSolutionsCopy.Add(vars[i].possibleSolutions[z]);
+                        }
+                        for(int j=0;j<possibleSolutionsCopy.Count;j++)
+                        {
+                            Console.WriteLine(possibleSolutionsCopy.Count);
+                            List<bool> solution = possibleSolutionsCopy[j];
+                            for(int k =0; k<value.Count;k++)
+                            {
+                                if (value[k] != solution[index])
+                                {
+                                    vars[i].possibleSolutions.RemoveAll(n=> n==possibleSolutionsCopy[j]);
+                                }
+                            }
+                        }
+                        if (vars[i].possibleSolutions.Count == 0)
+                        {
+                           
+                            return false;
+                        }
+                    }
+                    
+                }
+                return true;
                 
-                if (level == vars.Count)
+            }
+            bool backtrack(List<Line> vars, int level,List<Solution> sol)
+            {
+
+                Console.WriteLine(level);
+                fileTest.WriteLine(level);
+                if(level == vars.Count)
                 {
                     return true;
                 }
-                int currentIndex = vars[level].index;
-             
-                Console.WriteLine("level " + level);
-                
 
-                for (int i = 0; i < vars[level].possibleSolutions.Count; i++)
+                Line l = vars[level];
+                List<List<bool>> possibilities = new List<List<bool>>();
+                List<bool> value;
+                for(int i=0;i<l.possibleSolutions.Count;i++)
                 {
-                    
-                    bool isValid = valid(vars[level].lineType, vars[level].index, vars[level].possibleSolutions[i], solution);
-                    
-
-                    if (!isValid)
-                        continue;
-                    
-                    solution.Add(vars[level]);
-                    solution[solution.Count - 1].currentSolution = vars[level].possibleSolutions[i];
-                    bool recursiveCall = backtrack(vars, ++level, solution);
-                    if(recursiveCall)
-                    {
-                        return true;
-                    }
-                    else
-                    {
-
-                        solution.RemoveAll(item => item.index == currentIndex);
-
-                    }
-
-
+                    if(l.possibleSolutions[i].Count!=0)
+                        possibilities.Add(l.possibleSolutions[i]);
                 }
 
-
+                for(int i=0;i <possibilities.Count;i++)
+                {
+                    value = possibilities[i];
+                    int idx = sol.FindIndex(n => n.index == l.index && n.lineType == l.lineType);
+                    sol[idx].solution = value;
+                    sol[idx].assigned = true;
+                    if (valid(l.lineType, l.index, value, sol))
+                    {
+                        
+                        if (backtrack(vars,level+1,sol))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                int idx2 = sol.FindIndex(n => n.index == l.index && n.lineType == l.lineType);
+                defaultSolution.Clear();
+                for(int i=0;i<possibilities[0].Count;i++)
+                {
+                    defaultSolution.Add(false);
+                }
+                sol[idx2].solution = defaultSolution;
+                sol[idx2].assigned = false;
                 return false;
             }
 
-            backtrack(variables, 0, assignment);
-            foreach(Line l in variables)
+            backtrack(variables, 0,assignment);
+            foreach (Solution l in assignment)
             {
                 Console.WriteLine(l.index);
                 Console.WriteLine(l.lineType);
-                foreach(bool b in l.currentSolution)
+                foreach (bool b in l.solution)
                 {
                     if (b)
                         Console.Write("#");
@@ -181,6 +242,53 @@ namespace NonogramSolver
                 }
                 Console.WriteLine();
             }
+
+            Bitmap output = new Bitmap(grid.numOfCols, grid.numOfRows);
+            List<Solution> toDraw = new List<Solution>();
+            
+            foreach(Solution s in assignment)
+            {
+                if (s.lineType == LineType.row)
+                    toDraw.Add(s);
+            }
+            
+            for(int i =0;i<toDraw.Count;i++)
+            {
+                for(int j=0;j<toDraw[i].solution.Count;j++)
+                {
+                    if(toDraw[i].solution[j])
+                    {
+                        output.SetPixel(j, i, Color.Black);
+                    }
+                    else
+                    {
+                        output.SetPixel(j, i, Color.White);
+                    }
+                }
+            }
+
+            Bitmap resize(Bitmap img, int scale)
+            {
+                try
+                {
+                    Bitmap b = new Bitmap(img.Width * scale, img.Height * scale);
+                    using (Graphics g = Graphics.FromImage((Image)b))
+                    {
+                        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                        g.DrawImage(img, 0, 0, img.Width * scale, img.Height * scale);
+                    }
+                    return b;
+
+                }
+                catch
+                {
+                    Console.WriteLine("Could not resize");
+                    return img;
+                }
+            }
+
+            output = resize(output, 100);
+            output.Save("output.png");
 
 
         }
---
+++ NonogramSolver/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonogramSolver
+{
+    public class Solution
+    {
+        public List<bool> solution;
+        public LineType lineType;
+        public int index;
+        public bool assigned;
+
+       public Solution(List<bool> solution, LineType lineType, int index)
+        {
+            this.solution = solution;
+            this.lineType = lineType;
+            this.index = index;
+            this.assigned = false;
+        }
+    }
+}
